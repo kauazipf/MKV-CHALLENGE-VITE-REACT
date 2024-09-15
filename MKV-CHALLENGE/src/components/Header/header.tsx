f3a921d7502324d63d406b920f6ee7d74acae080
kauazipf <rm558957@fiap.com.br>
2024-09-14 22:22:55 -0300

    Realização e estlização do menu do cabeçalho com a utilização do useState.
    Co-authored-by: Victor Egidio Lira <Vitin46@users.noreply.github.com>
    Co-authored-by: marcelooou <marcelooou@users.noreply.github.com>

--- MKV-CHALLENGE/src/components/Header/header.tsx
+++ MKV-CHALLENGE/src/components/Header/header.tsx
@@ -3,23 +3,34 @@ import logo from './../../image/logo.png'
 import imgMenu from './../../image/menu.png'
 import imgLogin from './../../image/login.png'
 import style from './header.module.css'
+import { useState } from 'react';
 
 export default function Header(){
 
+    const [menuStyle, setMenuStyle] = useState({ display: 'none' });
+
+    const toggleMenu = () => {
+        if (menuStyle.display === 'block') {
+        setMenuStyle({ display: 'none' });
+        } else {
+        setMenuStyle({ display: 'block' });
+        }
+    };
+
     return(
         <header className={style.header}>
-            <img src={imgMenu} alt="Ícone Menu" className={style.icon} width={70} style={{left:20}}/>
+            <img src={imgMenu} alt="Ícone Menu" className={style.icon} width={70} style={{left:20}} onClick={toggleMenu}/>
 
             <img src={logo} alt="Logo" className={style.logo} width={120}/>
 
             <Link to={'/Login'}> <img src={imgLogin} alt="Ícone Login" className={style.icon} width={70} style={{right:20}}/> </Link>
 
-            <div className={style.menu} id="menu">
-                <Link to='/'> Home </Link>
-                <Link to='/Carros'> Carros </Link>
-                <Link to='/Chatbot'> Chatbot </Link>
-                <Link to='/Servicos'> Serviços </Link>
-                <Link to='/Participantes'> Participantes </Link>
+            <div className={style.menu} id="menu"  style={menuStyle}>
+                <Link to='/' className={style.link}> Home </Link>
+                <Link to='/Carros' className={style.link}> Carros </Link>
+                <Link to='/Chatbot' className={style.link}> Chatbot </Link>
+                <Link to='/Servicos' className={style.link}> Serviços </Link>
+                <Link to='/Participantes' className={style.link}> Participantes </Link>
             </div>
         </header>
     )
